e297bea90ad8b58376a659b379a0f2db1e6e1b42
tutoojoe <tutoojoephilip@gmail.com>
2022-03-02 14:57:50 +0530

    Header component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import Header from './components/Layout/Header'
+
+
+
+function App() {
+  return (
+    <Fragment>
+      <h2>Let's get started!</h2>
+      <Header></Header>
+      </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+
+import mealsImage from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button></button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Meals image" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
