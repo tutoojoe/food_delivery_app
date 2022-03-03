4ca9c74ffa1eb0e0a1f491bca8167809a835ba16
tutoojoe <tutoojoephilip@gmail.com>
2022-03-03 08:30:21 +0530

    mealItemform corrections

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -10,9 +10,9 @@ const MealItemForm = (props) => {
           id: "amount",
           type: "number",
           min: "1",
-            max: "5",
-            step: '1',
-          defaultValue:'1',
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
